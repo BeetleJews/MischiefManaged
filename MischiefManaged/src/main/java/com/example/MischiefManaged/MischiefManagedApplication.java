a95f9ee9ce0819321349216ba4d528f032443fa4
beetlejews <scoch56@gmail.com>
2019-02-17 19:10:35 +0300

    first commit

---
+++ MischiefManaged/src/main/java/com/example/MischiefManaged/MischiefManagedApplication.java
@@ -0,0 +1,13 @@
+package com.example.MischiefManaged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MischiefManagedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MischiefManagedApplication.class, args);
+	}
+
+}
